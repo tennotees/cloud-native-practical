0b92402e7c94a3ec10b83721b70afb92bdbcb651
Martijn Henckens <martijn.henckens@kbc.be>
2019-09-30 11:23:41 +0200

    adapted standard response status

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
@@ -1,9 +1,11 @@
 package com.ezgroceries.shoppinglist.controller;
 
 import com.ezgroceries.shoppinglist.resource.ShoppingList;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingListController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ShoppingList creteShoppingList(@RequestParam String name) {
         return new ShoppingList(name);
     }
