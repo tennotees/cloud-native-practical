6184c059d480b1c14815373f799ace04d8bbc290
Martijn Henckens <martijn.henckens@kbc.be>
2019-10-04 12:16:52 +0200

    shopping API tests added

--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/controller/ShoppingListControllerTests.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/controller/ShoppingListControllerTests.java
@@ -1,20 +1,24 @@
 package com.ezgroceries.shoppinglist.controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -24,6 +28,8 @@ public class ShoppingListControllerTests {
 
     @Autowired
     private MockMvc mvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void testCreateList_ResponseStatusCreated() throws Exception
@@ -70,33 +76,103 @@ public class ShoppingListControllerTests {
                 .param("name", "Stephanie's birthday"))
                 .andExpect(status().isCreated());
 
-        mvc.perform( MockMvcRequestBuilders
+        MvcResult result = mvc.perform( MockMvcRequestBuilders
                 .post("/shopping-lists/{listId}/cocktails" , listId)
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                 .content(objectMapper.writeValueAsString(cocktails))
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(result -> result.getResponse().getContentAsString().equals("{ cocktailId\": \"23b3d85a-3928-41c0-a533-6538a71e17c4\" }"));
+                .andReturn();
+
+        String resultAsString = result.getResponse().getContentAsString();
+        resultAsString = resultAsString.substring(1, resultAsString.length()-1);
+        Map<String, String> resultMap = objectMapper.readValue(resultAsString, Map.class);
+
+        assertEquals(resultMap.get("cocktailId"), cocktailId);
     }
 
     @Test
-    public void testAddCocktailsToList_BadRequestNoShoppingList() throws Exception
+    public void testGetList_ResponseOkAndListWithIngredientsReturned() throws Exception
     {
-        final String listId = "27912cbc-165e-4182-8491-23afb0e29919";
         final String cocktailId = "23b3d85a-3928-41c0-a533-6538a71e17c4";
         Map<String, String> cocktail = new HashMap<>();
         cocktail.put("cocktailId", cocktailId);
         List<Map> cocktails = new ArrayList<>();
         cocktails.add(cocktail);
-        ObjectMapper objectMapper = new ObjectMapper();
+
+        String listId1 = createShoppingList("Stephanie's birthday");
+        String listId2 = createShoppingList("My Birthday");
 
         mvc.perform( MockMvcRequestBuilders
-                .post("/shopping-lists/{listId}/cocktails" , listId)
+                .post("/shopping-lists/{listId}/cocktails" , listId1)
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                 .content(objectMapper.writeValueAsString(cocktails))
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isOk());
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/shopping-lists/{listId}", listId1))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-type", MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andReturn();
+
+        Map resultMap = objectMapper.readValue(result.getResponse().getContentAsString(), Map.class);
+        assertEquals(resultMap.get("shoppingListId"), "27912cbc-165e-4182-8491-23afb0e29919");
+        assertEquals(resultMap.get("name"), "Stephanie's birthday");
+        assertEquals(resultMap.get("ingredients"), Arrays.asList("Tequila", "Triple sec", "Lime juice", "Salt"));
+
     }
 
+    @Test
+    public void testGetAllLists_ResponseOkAndListsReturned() throws Exception
+    {
+        final String cocktailId = "23b3d85a-3928-41c0-a533-6538a71e17c4";
+        Map<String, String> cocktail = new HashMap<>();
+        cocktail.put("cocktailId", cocktailId);
+        List<Map> cocktails = new ArrayList<>();
+        cocktails.add(cocktail);
+
+        String listId1 = createShoppingList("Stephanie's birthday");
+        String listId2 = createShoppingList("My Birthday");
+
+        mvc.perform( MockMvcRequestBuilders
+                .post("/shopping-lists/{listId}/cocktails" , listId1)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(objectMapper.writeValueAsString(cocktails))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/shopping-lists"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-type", MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.content().string(JUnitMatchers.containsString("Stephanie's birthday")))
+                .andExpect(MockMvcResultMatchers.content().string(JUnitMatchers.containsString("My Birthday")));
+
+    }
+
+    private String createShoppingList(String name) throws Exception
+    {
+        MvcResult result =  mvc.perform(MockMvcRequestBuilders
+                .post("/shopping-lists")
+                .param("name", name))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+       Map<String, String> convertedResult = objectMapper.readValue(result.getResponse().getContentAsString(), Map.class);
+       return convertedResult.get("shoppingListId");
+    }
+
+    private String getCocktailId(String name) throws Exception
+    {
+        MvcResult result =  mvc.perform(MockMvcRequestBuilders
+                .get("/cocktails")
+                .param("name", name))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Map<String, String> convertedResult = objectMapper.readValue(result.getResponse().getContentAsString(), Map.class);
+        return convertedResult.get("cocktailId");
+    }
 }
 
